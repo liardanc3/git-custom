26c0288d6b20208db989ec3e4f68f0cd038402d0
liardanc3 <liardanc3@gmail.com>
2023-06-01 05:55:49 +0900

    Test environment setting

--- src/test/java/gitp/gitcustom/GitCustomApplicationTests.java
+++ src/test/java/gitp/gitcustom/GitCustomApplicationTests.java
@@ -3,22 +3,11 @@ package gitp.gitcustom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @SpringBootTest(classes= GitCustomApplication.class)
 class GitCustomApplicationTests {
 
-	@Test
-	void DateParseTest() throws ParseException {
-		String input = "2023-05-30 06:40:05 +0900";
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss Z");
+    @Test
+    void contextLoads(){
 
-		Date date = formatter.parse(input);
-
-		long priorityRank = date.getTime();
-		System.out.println("priorityRank = " + priorityRank);
-
-	}
+    }
 }
